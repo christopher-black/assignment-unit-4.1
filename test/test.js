2aef2f3e6ff6782658386fbcede3045542eebcb1
Christopher Black <chris@primeacademy.io>
2023-04-05 14:42:20 -0500

    Updated linter to check for spacing and var.

--- test/test.js
+++ test/test.js
@@ -1,19 +1,41 @@
 const assert = require('assert');
 const { expect } = require('chai');
 const { hello, helloName } = require('../assignment/scripts/function-practice.js');
+
+
 describe('Automated tests', function () {
-    describe(`hello()`, function () {
-        it(`should return the string 'hello'`, function () {
-            assert.equal(hello(), 'Hello World!');
-        });
+  describe(`should return the string 'hello'`, function () {
+    it(`should return the string 'hello'`, function () {
+      assert.equal(hello(), 'Hello World!');
     });
-    describe(`helloName()`, function () {
-        it(`should return the string 'Hello, name!'`, function () {
-            const result = helloName('Jane');
-            expect(result, 'helloName() does not return anything').to.exist;
-            result.should.be.a('string');
-            result.length.toBeGreaterThan(0);
-            assert.equal(hello(), 'Hello, Jane!');
-        });
+  });
+  describe(`should return the string 'Hello, Your Name!'`, function () {
+    it(`should return the string 'Hello, Your Name!'`, function () {
+      const result = helloName('Your Name');
+      expect(result, 'helloName() does not return anything').to.exist;
+      result.should.be.a('string');
+      result.length.toBeGreaterThan(0);
+      assert.equal(hello(), 'Hello, Your Name!');
     });
+  });
 });
+
+const lint = require('mocha-eslint');
+
+// Array of paths to lint
+// Note: a seperate Mocha test will be run for each path and each file which
+// matches a glob pattern
+const paths = [
+  'assignment/scripts/function-practice.js',
+];
+
+const options = {
+  // Consider linting warnings as errors and return failure
+  strict: true,  // Defaults to `false`, only notify the warnings
+
+  // Specify the mocha context in which to run tests
+  contextName: 'Code formatting',  // Defaults to `eslint`, but can be any string
+};
+
+// Run the tests
+lint(paths, options);
